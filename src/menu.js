260165b2cc322a0c9d22f52d59598a0735900860
Bhuwanesh Pareek <bhuwaneshpareek@gmail.com>
2017-08-10 22:39:34 +0530

    Update menu.js

--- src/menu.js
+++ src/menu.js
@@ -6,3 +6,18 @@ $(document).ready(function() {
     e.preventDefault();
   });
 });
+
+$(".faq-q").click( function () {
+  var container = $(this).parents(".faq-c");
+  var answer = container.find(".faq-a");
+  var trigger = container.find(".faq-t");
+  
+  answer.slideToggle(200);
+  
+  if (trigger.hasClass("faq-o")) {
+    trigger.removeClass("faq-o");
+  }
+  else {
+    trigger.addClass("faq-o");
+  }
+});
